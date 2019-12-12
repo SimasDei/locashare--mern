0b548ad462f62974b180b82883fc228e037aa2db
Simas <simonas.deimantavicius@mediapark.com>
2019-12-12 15:16:19 +0200

    add button component and styling

---
+++ src/shared/components/FormElements/Button/Button.component.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './Button.styles.scss';
+
+const Button = ({ href, children, size, inverse, danger, to, exact, type, onClick, disabled }) => {
+  if (href) {
+    return (
+      <a
+        className={`button button--${size || 'default'} ${inverse && 'button--inverse'} ${danger &&
+          'button--danger'}`}
+        href={href}>
+        {children}
+      </a>
+    );
+  }
+  if (to) {
+    return (
+      <Link
+        to={to}
+        exact={exact}
+        className={`button button--${size || 'default'} ${inverse && 'button--inverse'} ${danger &&
+          'button--danger'}`}>
+        {children}
+      </Link>
+    );
+  }
+  return (
+    <button
+      className={`button button--${size || 'default'} ${inverse && 'button--inverse'} ${danger &&
+        'button--danger'}`}
+      type={type}
+      onClick={onClick}
+      disabled={disabled}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
