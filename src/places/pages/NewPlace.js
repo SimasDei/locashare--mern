74fd731284318184cf9a34a5edb9d409dec8a898
Simas <simonas.deimantavicius@mediapark.com>
2019-12-13 12:38:29 +0200

    add versitile input component

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import Input from '../../shared/components/FormElements/Input';
+import './NewPlace.css';
 
 const NewPlace = () => {
   return (
     <form className='place-form'>
-      <Input type='text' label='Title' />
+      <Input type='text' label='Title' element='input' validators={[]} errorText="Please enter a valid title" />
     </form>
   );
 };
--- src/shared/components/FormElements/Input.jsx
+++ src/shared/components/FormElements/Input.jsx
@@ -1,18 +1,64 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
 import './Input.css';
 
+const actionTypes = {
+  CHANGE: 'CHANGE',
+};
+
+const inputReducer = (state, { type, value }) => {
+  switch (type) {
+    case actionTypes.CHANGE:
+      return {
+        ...state,
+        value,
+        isValid: true,
+      };
+    default:
+      return state;
+  }
+};
+
+const initialState = {
+  value: '',
+  isValid: false,
+};
+
 const Input = props => {
+  const [inputState, dispatch] = useReducer(inputReducer, initialState);
+
+  const handleChange = event => {
+    const {
+      target: { value },
+    } = event;
+    dispatch({ type: actionTypes.CHANGE, value });
+  };
+
   const element =
     props.element === 'input' ? (
-      <input id={props.id} type={props.type} placeholder={props.placeholder} />
+      <input
+        id={props.id}
+        type={props.type}
+        placeholder={props.placeholder}
+        onChange={handleChange}
+        value={inputState.value}
+      />
     ) : (
-      <textarea id={props.id} rows={props.rows || 3} />
+      <textarea
+        id={props.id}
+        rows={props.rows || 3}
+        onChange={handleChange}
+        value={inputState.value}
+      />
     );
+
+  const { id, label, errorText } = props;
+  const { isValid } = inputState;
   return (
-    <div className={`form-control`}>
-      <label htmlFor={props.id}>{props.label}</label>
+    <div className={`form-control ${!isValid && 'form-control--invalid'}`}>
+      <label htmlFor={id}>{label}</label>
       {element}
+      {!isValid && <p>{errorText}</p>}
     </div>
   );
 };
