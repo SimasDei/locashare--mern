615b1509d1a3008d861c219182c92603d6c99ebc
Simas <simonas.deimantavicius@mediapark.com>
2019-12-13 12:03:01 +0200

    add Goolge maps

--- src/shared/components/UIElements/Map.jsx
+++ src/shared/components/UIElements/Map.jsx
@@ -1,15 +1,19 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 
 import './Map.css';
 
 const Map = props => {
   const mapRef = useRef();
-  const map = new window.google.maps.Map(mapRef.current, {
-    center: props.center,
-    zoom: props.zoom,
-  });
+  const { center, zoom } = props;
 
-  new window.google.maps.Marker({ position: props.center, map });
+  useEffect(() => {
+    const map = new window.google.maps.Map(mapRef.current, {
+      center,
+      zoom,
+    });
+
+    new window.google.maps.Marker({ position: center, map });
+  }, [center, zoom]);
 
   return <div className={`map ${props.className}`} style={props.style} ref={mapRef}></div>;
 };
