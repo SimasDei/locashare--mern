aabe07bd0b571a2fea8792b6524088c92c319d11
SimasDei <simondeiman@gmail.com>
2019-12-14 19:56:46 +0200

    detructure props on Place item component

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -13,34 +13,36 @@ const PlaceItem = props => {
 
   const closeMapHandler = () => setShowMap(false);
 
+  const { address, coordinates, image, title, description, id } = props;
+
   return (
     <React.Fragment>
       <Modal
         show={showMap}
         onCancel={closeMapHandler}
-        header={props.address}
+        header={address}
         contentClass='place-item__modal-content'
         footerClass='place-item__modal-actions'
         footer={<Button onClick={closeMapHandler}>CLOSE</Button>}>
         <div className='map-container'>
-          <Map center={props.coordinates} zoom={16} />
+          <Map center={coordinates} zoom={16} />
         </div>
       </Modal>
       <li className='place-item'>
         <Card className='place-item__content'>
           <div className='place-item__image'>
-            <img src={props.image} alt={props.title} />
+            <img src={image} alt={title} />
           </div>
           <div className='place-item__info'>
-            <h2>{props.title}</h2>
-            <h3>{props.address}</h3>
-            <p>{props.description}</p>
+            <h2>{title}</h2>
+            <h3>{address}</h3>
+            <p>{description}</p>
           </div>
           <div className='place-item__actions'>
             <Button inverse onClick={openMapHandler}>
               VIEW ON MAP
             </Button>
-            <Button to={`/places/${props.id}`}>EDIT</Button>
+            <Button to={`/places/${id}`}>EDIT</Button>
             <Button danger>DELETE</Button>
           </div>
         </Card>
